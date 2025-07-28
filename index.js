17c296b03622d0156de7fe87c4e20b61781c4572
Harry Dunnie <peteranietieh@gmail.com>
2025-07-28 14:33:56 +0100

    refactoring code

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const notes = [
   {
     id: "123e4567-e89b-12d3-a456-426614174000", // hardcoded UUID // uuidv4()
     title: "First Note",
-    content: "This is the first note.",
+    body: "This is the first note.",
     createdAt: Date.now(),
   },
   {
     id: "123e4567-e89b-12d3-a456-426614174001",
     title: "Second Note",
-    content: "This is the second note.",
+    body: "This is the second note.",
     createdAt: Date.now(),
   },
 ];
@@ -21,8 +21,8 @@ const notes = [
 
 const server = http.createServer((req, res) => {
   const { url, method } = req;
-  const notesUUIDRegex = /^\/notes\/[0-9a-fA-F-]{36}$/;
 
+  const notesUUIDRegex = /^\/notes\/[0-9a-fA-F-]{36}$/;
   // Set response header
   res.setHeader("Content-Type", "application/json");
 
@@ -31,28 +31,30 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.end(JSON.stringify(notes));
   } else if (notesUUIDRegex.test(url) && method === "GET") {
-    /// Respond with a note ID
-    // Get ID
+    // Get ID from URL
     const urlId = url.split("/")[2];
 
-    ///get note element that match particular id
-
+    ///find note element that match id from reqeust
     const noteFetched = notes.find((e) => e.id === urlId);
 
-    // respond with note
-    res.statusCode = 200;
-    res.end(JSON.stringify(noteFetched));
+    // respond with note if found
+    if (noteFetched) {
+      res.statusCode = 200;
+      res.end(JSON.stringify(noteFetched));
+    } else {
+      res.statusCode = 400;
+      res.end(JSON.stringify({ error: "Note Not Found" }));
+    }
   } else if (url === "/notes" && method === "POST") {
     let body = "";
 
-    // Listen for data chunks
+    // Listen for data chunks and collect
     req.on("data", (chunk) => {
       console.log(chunk);
       body += chunk.toString(); // convert buffer to string
     });
 
     // All data recieved
-
     req.on("end", () => {
       try {
         const data = JSON.parse(body); // Parse the string
@@ -60,19 +62,12 @@ const server = http.createServer((req, res) => {
         console.log("Parsed data:", data);
 
         /// store to notes array, give it an ID, and the time it was created
+        const newlyCreatedNote = processData(data);
 
-        const processData = function (inputData) {
-          // give the data an id and date created
-          inputData.id = uuidv4();
-          inputData.createdAt = new Date().getTime();
-          console.log(inputData);
-          notes.push(inputData);
-        };
-        processData(data);
-        res.writeHead(201, { "content-type": "application/json" });
-        res.end(JSON.stringify({ message: "Note created", data }));
+        res.statusCode = 201;
+        res.end(JSON.stringify({ message: "Note created", newlyCreatedNote }));
       } catch (error) {
-        res.writeHead(400, { "Content-Type": "application/json" });
+        res.statusCode = 400;
         res.end(JSON.stringify({ error: "Invalid JSON" }));
         console.log(error);
       }
@@ -87,16 +82,49 @@ const server = http.createServer((req, res) => {
     });
     console.log(body);
     try {
+      let updatedNote;
       req.on("end", () => {
         const data = JSON.parse(body);
-        const noteToWorkOn = notes.find((e) => e.id === data.id);
-        const updateNote = function (updatedData) {
-          updatedData.title;
-        };
+
+        ///
+        updatedNote = updateNote(data);
       });
-    } catch (error) {}
+
+      //Respond with updated note
+      res.statusCode = 201;
+      res.end(JSON.stringify({ message: "Updated sucessfull", updatedNote }));
+    } catch (error) {
+      res.statusCode = 400;
+      res.end(
+        JSON.stringify({ error: "Couldn't update note. Please try again" })
+      );
+      console.log(error);
+    }
   }
 });
+
+////Function working on data
+// create store new data
+const processData = function (inputData) {
+  const newNote = inputData;
+  // give the data an id and date created
+  newNote.id = uuidv4(); //"123e4567-e89b-12d3-a456-426614174002"
+  newNote.createdAt = new Date().getTime();
+  console.log(newNote);
+  notes.push(newNote);
+  return newNote;
+};
+
+////update existing note
+
+const updateNote = function (updatedData) {
+  //First find the notes to update
+  const noteToWorkOn = notes.find((e) => e.id === updatedData.id);
+  //update the note
+  noteToWorkOn.title = updatedData.title;
+  noteToWorkOn.body = updatedData.body;
+  return noteToWorkOn;
+};
 
 /// Start server on port 5000
 
