c5d717741c7e3f06fbc390a2d532570749fc62d2
Harry Dunnie <peteranietieh@gmail.com>
2025-07-29 11:53:43 +0100

    modified code and add delete request to api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const { title } = require("process");
 const { v4: uuidv4 } = require("uuid");
 
 const notes = [
@@ -62,7 +63,7 @@ const server = http.createServer((req, res) => {
         console.log("Parsed data:", data);
 
         /// store to notes array, give it an ID, and the time it was created
-        const newlyCreatedNote = processData(data);
+        const newlyCreatedNote = createNewNote(data);
 
         res.statusCode = 201;
         res.end(JSON.stringify({ message: "Note created", newlyCreatedNote }));
@@ -78,52 +79,74 @@ const server = http.createServer((req, res) => {
 
     /// Listen to data
     req.on("data", (chunk) => {
-      body = +chunk.toString(); // parse the
+      body += chunk.toString(); // parse the
     });
     console.log(body);
     try {
-      let updatedNote;
       req.on("end", () => {
         const data = JSON.parse(body);
 
         ///
-        updatedNote = updateNote(data);
+        let updatedNote = updateNote(data);
+        //Respond with updated note
+        res.statusCode = 201;
+        res.end(JSON.stringify({ message: "Updated sucessfull", updatedNote }));
       });
-
-      //Respond with updated note
-      res.statusCode = 201;
-      res.end(JSON.stringify({ message: "Updated sucessfull", updatedNote }));
     } catch (error) {
+      console.log(error);
       res.statusCode = 400;
       res.end(
         JSON.stringify({ error: "Couldn't update note. Please try again" })
       );
-      console.log(error);
     }
+  } else if (notesUUIDRegex.test(url) && method === "DELETE") {
+    const urlId = url.split("/")[2];
+    deleteNote(urlId);
+
+    res.statusCode = 201;
+    res.end(JSON.stringify({ message: "Note deleted sucessfully" }));
+  } else {
+    res.statusCode = 400;
+    res.end(JSON.stringify({ message: "Not Found. Please check URL" }));
   }
 });
 
-////Function working on data
-// create store new data
-const processData = function (inputData) {
-  const newNote = inputData;
-  // give the data an id and date created
-  newNote.id = uuidv4(); //"123e4567-e89b-12d3-a456-426614174002"
-  newNote.createdAt = new Date().getTime();
-  console.log(newNote);
+////Functions working on data
+// create store new note
+const createNewNote = function (inputData) {
+  data = inputData;
+  const newNote = {
+    id: uuidv4(), //"123e4567-e89b-12d3-a456-426614174002",
+    title: data.title,
+    body: data.body,
+    createdAt: new Date().getTime(),
+  };
   notes.push(newNote);
+  console.log(newNote);
   return newNote;
 };
 
 ////update existing note
-
 const updateNote = function (updatedData) {
+  const newUpdatedNote = updatedData;
+  console.log(newUpdatedNote);
   //First find the notes to update
-  const noteToWorkOn = notes.find((e) => e.id === updatedData.id);
+  const noteToWorkOn = notes.find((e) => e.id === newUpdatedNote.id);
+  console.log(noteToWorkOn);
   //update the note
-  noteToWorkOn.title = updatedData.title;
-  noteToWorkOn.body = updatedData.body;
+  noteToWorkOn.title = newUpdatedNote.title;
+  noteToWorkOn.body = newUpdatedNote.body;
+  noteToWorkOn.createdAt = `edited at: ${new Date().getTime()}`;
   return noteToWorkOn;
+};
+
+/// Delete note
+const deleteNote = function (noteId) {
+  const id = noteId;
+  const noteToDelete = notes.findIndex((note) => note.id === id);
+  console.log(noteToDelete);
+  const deleted = notes.splice(noteToDelete, 1);
+  console.log(deleted);
 };
 
 /// Start server on port 5000
